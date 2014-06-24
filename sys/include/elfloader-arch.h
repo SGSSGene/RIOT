47e3adfd7ba0e8c313dc0e6017025523ff56ae04
Simon Gene Gottlieb <gene@staubsaal.de>
2014-06-24 23:34:31 +0200

    .

--- sys/include/elfloader-arch.h
+++ sys/include/elfloader-arch.h
@@ -88,7 +88,7 @@
  */
 void elfloader_arch_relocate(void * fd, unsigned int sectionoffset,
 			     char *sectionaddr,
-			     elf32_rela_t* rela, char *addr);
+			     elf32_rela_t* rela, const char *addr);
 
 
 #endif /* ELFLOADER_ARCH_H_ */
